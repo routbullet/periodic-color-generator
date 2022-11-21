e2eab5bc41d9bedcfa828863abf7ded96bb07dca
Bullet Rout <bulletrout@Bullets-MacBook-Air.local>
2022-11-21 20:07:46 +0530

    utils and copy to clipboard ready

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { rgbToHex } from "../utils/rgbToHex";
+import "./Color.style.scss";
 
-const ColorComponent = () => {
-  return <div>ColorComponent</div>;
+const ColorComponent = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const hexCode = rgbToHex(rgb);
+  const JOIN_RGB = rgb.join(",");
+  const RGB_TEXT = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+
+  return (
+    <article
+      className={`color`}
+      style={{ backgroundColor: `rgb(${JOIN_RGB})` }}
+    >
+      <h6 className={`${index > 10 ? "text-color" : null}`}>{weight}%</h6>
+      <h6
+        onClick={() => {
+          setAlert(true);
+          navigator.clipboard.writeText(RGB_TEXT);
+        }}
+        className={`${index > 10 ? "text-color" : null}`}
+      >
+        {RGB_TEXT}
+      </h6>
+      <h6
+        onClick={() => {
+          setAlert(true);
+          navigator.clipboard.writeText(hexCode);
+        }}
+        className={`${index > 10 ? "text-color" : null}`}
+      >
+        {hexCode}
+      </h6>
+      {alert && (
+        <h6 className={`${index > 10 ? "text-color" : null}`}>Copied</h6>
+      )}
+    </article>
+  );
 };
 
 export default ColorComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Values from "values.js";
+import ColorComponent from "../components/Color";
+import "./Home.style.scss";
 
 const HomeContainer = () => {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [colorList, setColorList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let newColor = new Values(color).all(10);
+      setColorList(newColor);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
-    <main>
-      <h2>Color generator home</h2>
-    </main>
+    <>
+      <section className="form-section">
+        <h2>PERIODIC COLOR GENERATOR</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#fff124"
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? "error-value" : null}`}
+          />
+          <button className="submit-button" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors-section">
+        {colorList.map((colors, index) => {
+          console.log(colors);
+          return <ColorComponent key={index} {...colors} index={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
